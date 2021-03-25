0fd21c654386b18f1175cead85047bd87622b73b
Jacob <19372006+starsflower@users.noreply.github.com>
2021-03-25 12:30:52 +0000

    More info and styling

--- src/Schedule.tsx
+++ src/Schedule.tsx
@@ -14,13 +14,27 @@ export default function Schedule(props: ScheduleData) {
     let [value, setValue] = useStickyState<ScheduleData>(props, props.id);
     return (
         <div className="Schedule">
-            <div className="MainBar">
-                <div className="Title">
-                    {value.title}
+            <div className="Info">
+                <div className="MainBar">
+                    <div className="Title">
+                        {value.title}
+                    </div>
+                    <div className="Countdown">
+                        <Moment date={value.date} fromNow withTitle></Moment>
+                    </div>
                 </div>
-                <div className="Countdown">
-                    <Moment date={value.date} fromNow></Moment>
+                <div className="InfoBar">
+                    <div className="Date">
+                        <Moment date={value.date} local></Moment>
+                    </div>
+
+                    <div className="Description">
+                        {value.description}
+                    </div>
                 </div>
+            </div>
+            <div className="Actions">
+                aaa
             </div>
         </div>
     );
